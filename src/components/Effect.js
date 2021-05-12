83760778197a658d6d21f8f78e3f4af16f10e0cb
EditaGerv <editagervinaite@yahoo.com>
2021-05-13 00:22:42 +0300

    veikia

---
+++ src/components/Effect.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+function Effect() {
+  const [paieskosZodis, setPaieskosZodis] = useState('users');
+  const [gautaInfo, setGautaInfo]= useState([]);
+
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${paieskosZodis}`)
+    .then(response => response.json())
+    .then(json => setGautaInfo(json))
+  }, [paieskosZodis])
+
+
+  return (
+    <div>
+      Effect
+      <div>
+        <button onClick={()=> setPaieskosZodis('posts')}>posts</button>
+        <button onClick={()=> setPaieskosZodis('users')}>users</button>
+        <button onClick={()=> setPaieskosZodis('comments')}>comments</button>
+      </div>
+      <div>
+        <h2>{paieskosZodis}</h2>
+        {gautaInfo.map(item => {
+          return <p>{JSON.stringify(item)}</p>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Effect;
